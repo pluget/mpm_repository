8273aa79812fba92733c56ae25c11e123c392cca
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2022-04-21 19:06:00 +0200

    Update to spigotmc.org scraper

--- scripts/spigotmc.org.py
+++ scripts/spigotmc.org.py
@@ -1,17 +1,23 @@
+import re
 import time
 from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
 
 driver = uc.Chrome()
 
-page = driver.get('https://www.spigotmc.org/resources/categories/spigot.4/?order=download_count&page=1')
+page = driver.get('https://spigotmc.org/resources/categories/spigot.4/?order=download_count&page=1')
 time.sleep(6)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 items = soup.select("form ol.resourceList li.resourceListItem")
 itemsObject = []
 for i in items:
-    itemsObject.append([i.select("h3.title > a")[0].get("href"), i.select("h3.title > a")[0].getText()])
+    itemsObject.append([i.select("h3.title > a")[0].get("href"), i.select("h3.title > a")[0].getText(), list(map(lambda x:x.lower(), re.findall(r'[a-zA-Z]+', i.select("h3.title > a")[0].getText())))])
+    print(itemsObject)
 
-for i in itemsObject:
-    print(i[1])
+for i, x in enumerate(itemsObject):
+    page = driver.get("https://spigotmc.org/" + x[0])
+    # items = soup.select("")
+
+time.sleep(6)
+
