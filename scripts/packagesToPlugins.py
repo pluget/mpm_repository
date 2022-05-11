02f169eacaa58db3a45f06e54b35af6e62db98e8
mbledkowski <pub@mble.dk>
2022-05-11 18:34:59 +0200

    packagesToPlugins is now fully working

--- scripts/packagesToPlugins.py
+++ scripts/packagesToPlugins.py
@@ -36,7 +36,10 @@ session.headers = {"Authorization": f"token {access_token}"}
 f = open("../repository/packages.json")
 dictF = json.load(f)
 
+print("{")
+
 for i in dictF:
+  print(f"\"{i}\": " + "{")
   url = dictF[i]["gitUrl"].split("/")
   if len(url) > 4 and url[2] == "github.com":
     logging.error(f"{url[3]}/{url[4]}")
@@ -44,6 +47,20 @@ for i in dictF:
     dictReq = json.loads(req.text)
     if "message" not in dictReq:
       for j in dictReq:
-        print(j["tag_name"])
-        for k in j["assets"]:
-          k["name"]
+        tag_name = j["tag_name"]
+        git_url = dictF[i]["gitUrl"]
+        assets_url = ""
+        if len(j["assets"]) > 0:
+          assets_url = j["assets"][0]["url"]
+          download_url = session.get(assets_url).json()["browser_download_url"]
+        print(f"\"{tag_name}\": " + "{")
+        print("\"about\": {")
+        print("\"type\": \"github\",")
+        print(f"\"sourceUrl\": \"{git_url}\",")
+        print(f"\"downloadUrl\": \"{download_url}\",")
+        print("},\n\"downloadIpfsHash\": \"\",\n\"supportedApis\": [\"spigot\", \"paper\", \"glowkit\"],\n\"gameVersions\": [],\n\"dependencies\": []\n},")
+        if len(j["assets"]) > 1:
+          logging.error(f"More than one asset: {tag_name}")
+  print("},")
+
+print("}")
