d3e827fab2dce9340b5981c7d16041d1bb234dd9
Maciej Błędkowski <pub@mble.dk>
2022-05-20 03:29:29 +0200

    veridToIpfs should be now fully working

--- scripts/veridToIpfs/src/index.ts
+++ scripts/veridToIpfs/src/index.ts
@@ -13,57 +13,99 @@ puppeteer.use(StealthPlugin());
 
 // Prepare
 
+// Checks when file is downloaded, returns file data and path
+
+async function checkIfFileIsDownloaded(
+  downloadPath: string,
+  iterations?: number
+): Promise<{
+  data: Buffer | null;
+  path: string;
+}> {
+  iterations = iterations || 0;
+
+  // Wait 500 ms
+  await new Promise((resolve, reject) => setTimeout(resolve, 500));
+
+  if (iterations > 35) {
+    return { data: null, path: "" };
+  }
+
+  const files = await fs.readdir(downloadPath);
+  const file = path.resolve(downloadPath, "" + files[0]);
+
+  // Check if jar file is downloaded
+  if (path.extname(file) === ".jar") {
+    // Return buffer with file content
+    const fileData = await fs.readFile(path.resolve(downloadPath, files[0]));
+    return { data: fileData, path: file };
+  } else {
+    return await checkIfFileIsDownloaded(downloadPath, iterations + 1);
+  }
+}
+
 // Initialize NFT.storage client
 const NFT_STORAGE_TOKEN = process.env.NFT_STORAGE_TOKEN || "";
 const client = new NFTStorage({ token: NFT_STORAGE_TOKEN });
 
-const browser = await puppeteer.launch({
-  headless: false,
-  executablePath: "/run/current-system/sw/bin/google-chrome-stable",
-});
-const page = (await browser.pages())[0];
+// Uploads file to IPFS, removes file, returns CID
 
-const downloadPath = path.resolve("./downloads");
-const pageClient = await page.target().createCDPSession();
-await pageClient.send("Page.setDownloadBehavior", {
-  behavior: "allow",
-  downloadPath: downloadPath,
-});
+async function fileToCid(file: {
+  data: Buffer | null;
+  path: string;
+}): Promise<string | null> {
+  if (file.data) {
+    const blob = new Blob([file.data]);
+    const cid = await client.storeBlob(blob);
 
-const id = 32761;
-const url = "resources/powernbt.9098/download?version=32761";
+    fs.unlink(file.path);
 
-const response = await page.goto(`https://spigotmc.org/${url}`);
-
-await page.waitForTimeout(5200);
-
-async function checkIfFileIsDownloaded() {
-  try {
-    await new Promise((resolve, reject) => setTimeout(resolve, 500));
-    const files = await fs.readdir(downloadPath);
-    console.log(files);
-    if (path.extname(files[0]) === ".jar") {
-      const file = await fs.readFile(files[0]);
-      return file;
-    } else {
-      await checkIfFileIsDownloaded();
-    }
-  } catch {
-    await checkIfFileIsDownloaded();
+    return cid;
   }
+  return null;
 }
 
-const file = await checkIfFileIsDownloaded();
+// Launch webbrowser
+const CHROME_PATH = process.env.CHROME_PATH || "";
+const browser = await puppeteer.launch({
+  headless: false,
+  executablePath: CHROME_PATH,
+});
+// Use first tab
+const page = (await browser.pages())[0];
 
-console.log(file, typeof file);
-// const imageFile = new File([blob], `${id}.jar`, {
-//   type: "application/jar",
-// });
+const pageClient = await page.target().createCDPSession();
+const downloadPath = path.resolve("./downloads");
+await pageClient.send("Page.setDownloadBehavior", {
+  behavior: "allow",
+  downloadPath,
+});
 
-// const metadata = await client.store({
-//   name: url,
-//   description: "Uploaded using mpm_repository/scripts/veridToIpfs",
-//   image: imageFile,
-// });
+const nameRepository = await fs.readFile(
+  path.resolve("../../repository/name.json")
+);
+const nameRepositoryDict = JSON.parse("" + nameRepository);
+const veridToCid: { [key: number]: string } = {};
+for (const i in nameRepositoryDict) {
+  const id = nameRepositoryDict[i].spigot;
+  const res = await fetch(
+    `https://api.spiget.org/v2/resources/${id}/versions?size=9999`
+  );
+  const versions: { id: number; url?: string }[] = await res.json();
+  for (let i = 0; i < versions.length; i++) {
+    const version = versions[i];
+    const verid = version.id;
+    const url = version.url;
+    if (url !== undefined) {
+      console.log(url);
 
-// console.log(metadata);
+      page.goto(`https://spigotmc.org/${url}`);
+
+      Object.assign(veridToCid, {
+        [verid]: await fileToCid(await checkIfFileIsDownloaded(downloadPath)),
+      });
+      await fs.writeFile("../../repository/verid.json", veridToCid);
+    }
+  }
+}
+await browser.close();
