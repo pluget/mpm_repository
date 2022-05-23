e409efeb2f96f5cff5b94e7786c0913e6d139ed6
Maciej Błędkowski <pub@mble.dk>
2022-05-23 15:00:26 +0200

    Fix to veridToIpfs

--- scripts/veridToIpfs/src/index.ts
+++ scripts/veridToIpfs/src/index.ts
@@ -27,7 +27,11 @@ async function checkIfFileIsDownloaded(downloadPath: string): Promise<{
   const file = path.resolve(downloadPath, "" + files[0]);
 
   // Check if jar or sk or zip file is downloaded
-  if (path.extname(file) === ".jar" || path.extname(file) === ".sk" || path.extname(file) === ".zip") {
+  if (
+    path.extname(file) === ".jar" ||
+    path.extname(file) === ".sk" ||
+    path.extname(file) === ".zip"
+  ) {
     // Return buffer with file content
     const fileData = await fs.readFile(path.resolve(downloadPath, files[0]));
     return { data: fileData, path: file };
@@ -97,7 +101,9 @@ for (let i = 0; i < 6; i++) {
 
 const namesRaw = await fs.readFile(path.resolve("../../repository/name.json"));
 const names = JSON.parse("" + namesRaw);
-const veridCid: { [key: number]: string } = await fs.readJson("../../repository/verid.json")
+const veridCid: { [key: number]: string } = await fs.readJson(
+  "../../repository/verid.json"
+);
 let pagePromises: Promise<void>[] = new Array();
 let i = 0;
 for (const name in names) {
@@ -119,30 +125,32 @@ for (const name in names) {
 
         async function loadPage() {
           const version = versions[j];
-          const verid = version.id;
+          const verid = versions[j].id;
           const url = version.url;
           if (url !== undefined) {
             console.log(url);
 
             try {
-              const waitForPagePromise = new Promise<void>((resolve, reject) => {
-                // await page.goto(url, { waitUntil: "networkidle0" }); timeout after 30 sek
-                async function waitForPage() {
-                  try {
-                    await page.goto(`https://spigotmc.org/${url}`, {
-                      waitUntil: "networkidle0",
-                    });
-                  } catch (e) {
-                    console.log(e);
+              const waitForPagePromise = new Promise<void>(
+                (resolve, reject) => {
+                  // await page.goto(url, { waitUntil: "networkidle0" }); timeout after 30 sek
+                  async function waitForPage() {
+                    try {
+                      await page.goto(`https://spigotmc.org/${url}`, {
+                        waitUntil: "networkidle0",
+                      });
+                    } catch (e) {
+                      console.log(e);
+                    }
+                    resolve();
                   }
-                  resolve();
-                }
 
-                waitForPage();
-                setTimeout(() => {
-                  reject();
-                }, 30000);
-              });
+                  waitForPage();
+                  setTimeout(() => {
+                    reject();
+                  }, 30000);
+                }
+              );
               await waitForPagePromise;
             } catch (e) {
               console.log(e);
